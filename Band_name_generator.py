7ddd5a3e174659503f6a443f1badfe5f736fa05f
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2022-12-04 08:32:24 +0530

    Add files via upload

---
+++ Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator")
+name_place=input("what is the name of the city you grew up in? ")
+pet_name=input("what is the name of your pet? ")
+print(("your Band name could be ") + name_place + " " + pet_name)
