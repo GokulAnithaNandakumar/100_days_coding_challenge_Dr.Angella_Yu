f15ed514145d873dcb866568618394303595d613
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2022-12-04 08:36:59 +0530

    Update Band_name_generator.py

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,3 +1,4 @@
+# Band Name generator is a fun beginner level project which deals with basic like strings and merging strings
 print("Welcome to band name generator")
 name_place=input("what is the name of the city you grew up in? ")
 pet_name=input("what is the name of your pet? ")
